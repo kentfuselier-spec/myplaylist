580f2c6a07414cb2eddb2a93e2346dc30829330b
kentfuselier-spec <kent.fuselier@airliquide.com>
2025-12-26 13:18:49 -0600

    Update playlist.sh

--- playlist.sh
+++ playlist.sh
@@ -8,7 +8,7 @@ if [[ -f $path && -f $sourcefile ]]; then
     echo "The contents of playlist.txt should be seperated by genera which is specified by"
     echo "# Genera"
     echo "each subsequent line in that genera needs to be specified like this"
-    echo "Song Title $$ Artist $$ YT link"
+    echo "Song Title \$\$ Artist \$\$ YT link"
     echo "After this file is made, rerun the script"
     echo ""
   else
@@ -18,7 +18,7 @@ if [[ -f $path && -f $sourcefile ]]; then
 elif [[ -f $path ]]; then
   echo "Need a sourcefile 'playlist.txt' in active dir. See './playlist.sh help' for more info."
 elif [[ -f $sourcefile ]]; then
-  echo "Need to specifiy which dir the finished mp3 are to be placed. './playlist.sh $HOME/Downloads' would be used to place all music in the download folder."
+  echo "Need to specifiy which dir the finished mp3 are to be placed. './playlist.sh \$HOME/Downloads' would be used to place all music in the download folder."
 else
   echo "Need a sourcefile 'playlist.txt' in active dir. See './playlist.sh help' for more info."
 fi
