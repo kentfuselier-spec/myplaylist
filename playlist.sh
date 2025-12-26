0f5d806cfa60354303b447054057598098297581
kentfuselier-spec <kent.fuselier@airliquide.com>
2025-12-26 13:13:48 -0600

    Update playlist.sh

--- playlist.sh
+++ playlist.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+path=$1
+sourcefile="./playlist.txt"
+
+if [[ -f $path && -f $sourcefile ]]; then
+  if [[ $1 == "help" ]]; then
+    echo "This script looks for a 'playlist.txt' to download and convert music."
+    echo "The contents of playlist.txt should be seperated by genera which is specified by"
+    echo "# Genera"
+    echo "each subsequent line in that genera needs to be specified like this"
+    echo "Song Title $$ Artist $$ YT link
+    echo "After this file is made, rerun the script"
+    echo ""
+  else
+    ## Actual script starts here 
+    ## Actual script ends here
+  fi
+elif [[ -f $path ]]; then
+  echo "Need a sourcefile 'playlist.txt' in active dir. See './playlist.sh help' for more info."
+elif [[ -f $path && -f $sourcefile ]]; then
+  echo "Need to specifiy which dir the finished mp3 are to be placed. './playlist.sh $HOME/Downloads' would be used to place all music in the download folder."
+fi
